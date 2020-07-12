6a138cba9873e3c93e2314473296a079024fb4c8
kavin_tian@163.com <kavin_tian@163.com>
2020-07-12 16:24:20 +0800

    test branch

---
+++ app/src/main/java/com/example/toggletheme/Test.java
@@ -0,0 +1,12 @@
+package com.example.toggletheme;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Test extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
